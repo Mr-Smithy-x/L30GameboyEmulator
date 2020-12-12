bb1fd1628d765dbd23d25fb165b7a8106e994100
Charlton Smith <CharltonSmith@outlook.com>
2020-12-12 01:05:34 -0500

    CPU tests

--- src/com/vonley/registers/CPU.kt
+++ src/com/vonley/registers/CPU.kt
@@ -5,7 +5,6 @@ object CPU {
     //HI            //LO
     var a: Int = 0 //Accumulator & Flags
 
-
     var b: Int = 0; var c: Int = 0  //BC
     var d: Int = 0; var e: Int = 0  //DE
     var h: Int = 0; var l: Int = 0  //HL
@@ -15,8 +14,20 @@ object CPU {
     //Program Counter
     var pc: Int = 0
 
+    val hl: Int
+        get() {
+            //(h << 8) | l
+            return (h shl 8) or l
+        }
 
+    val de: Int
+        get() {
+            return (d shl 8) or e
+        }
 
-
+    val bc: Int
+        get() {
+            return (b shl 8) or c
+        }
 
 }
--- src/tests/CPUTest.kt
+++ src/tests/CPUTest.kt
@@ -1,25 +1,34 @@
 package tests
 
+import com.vonley.registers.CPU
 import org.junit.Test
-
-class CPUTest{
-
-
-
+/**
+ * 0 - 8 signed range is -128 - 127
+ * 0 - 8 unsigned range is 0-256
+ * Reference Bitwise Test class for understanding
+ * @see BitWiseTests
+ */
+class CPUTest {
 
     @Test
     fun testBC() {
-
+        CPU.b = 0xA8
+        CPU.c = 0x10
+        assert(CPU.bc == (CPU.b shl 8) or CPU.c)
     }
 
     @Test
     fun testDE() {
-
+        CPU.d = 0x3F
+        CPU.e = 0x7F
+        assert(CPU.de == (CPU.d shl 8) or CPU.e)
     }
 
     @Test
     fun testHL() {
-
+        CPU.h = 0x3F
+        CPU.l = 0x7F
+        assert(CPU.hl == (CPU.h shl 8) or CPU.l)
     }
 
 }
