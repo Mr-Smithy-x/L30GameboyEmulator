24a0fb481111fd87746187d61129b50ba4e85d01
Charlton Smith <CharltonSmith@outlook.com>
2020-12-12 01:30:16 -0500

    cpu test with setting hl, de, bc

--- src/com/vonley/registers/CPU.kt
+++ src/com/vonley/registers/CPU.kt
@@ -5,29 +5,39 @@ object CPU {
     //HI            //LO
     var a: Int = 0 //Accumulator & Flags
 
-    var b: Int = 0; var c: Int = 0  //BC
-    var d: Int = 0; var e: Int = 0  //DE
-    var h: Int = 0; var l: Int = 0  //HL
+    var b: Int = 0;
+    var c: Int = 0  //BC
+    var d: Int = 0;
+    var e: Int = 0  //DE
+    var h: Int = 0;
+    var l: Int = 0  //HL
 
     //Stack Pointer
     var sp: Int = 0
+
     //Program Counter
     var pc: Int = 0
 
-    val hl: Int
-        get() {
-            //(h << 8) | l
-            return (h shl 8) or l
+    var hl: Int
+        get() = (h shl 8) or l
+        set(value) {
+            h = (value shr 8)
+            l = (value and 0xFF)
         }
 
-    val de: Int
-        get() {
-            return (d shl 8) or e
+    var de: Int
+        get() = (d shl 8) or e
+        set(value) {
+            d = (value shr 8)
+            e = (value and 0xFF)
         }
 
-    val bc: Int
-        get() {
-            return (b shl 8) or c
+    var bc: Int
+        get() = (b shl 8) or c
+        set(value) {
+            b = (value shr 8)
+            c = (value and 0xFF)
         }
+
 
 }
--- src/tests/CPUTest.kt
+++ src/tests/CPUTest.kt
@@ -5,6 +5,7 @@ import org.junit.Test
 /**
  * 0 - 8 signed range is -128 - 127
  * 0 - 8 unsigned range is 0-256
+ * 0xFF = maxrange
  * Reference Bitwise Test class for understanding
  * @see BitWiseTests
  */
@@ -29,6 +30,52 @@ class CPUTest {
         CPU.h = 0x3F
         CPU.l = 0x7F
         assert(CPU.hl == (CPU.h shl 8) or CPU.l)
+    }
+
+    @Test
+    fun testSetBC(){
+        val b = 0x2F
+        val c = 0x18
+
+        CPU.b = b
+        CPU.c = c
+
+        assert(CPU.bc == (CPU.b shl 8) or CPU.c)
+        CPU.b = 0
+        CPU.c = 0
+        CPU.bc = 0x2F18
+        assert(CPU.b == b)
+        assert(CPU.c == c)
+    }
+
+    @Test
+    fun testSetDE(){
+        val d = 0x34
+        val e = 0x9F
+        CPU.d = d
+        CPU.e = e
+
+        assert(CPU.de == (CPU.d shl 8) or CPU.e)
+        CPU.d = 0
+        CPU.e = 0
+        CPU.de = 0x349F
+        assert(CPU.d == d)
+        assert(CPU.e == e)
+    }
+
+    @Test
+    fun testSetHL(){
+        val h = 0x27
+        val l = 0x4F
+
+        CPU.h = h
+        CPU.l = l
+        assert(CPU.hl == (CPU.h shl 8) or CPU.l)
+        CPU.h = 0
+        CPU.l = 0
+        CPU.hl = 0x274F
+        assert(CPU.h == h)
+        assert(CPU.l == l)
     }
 
 }
